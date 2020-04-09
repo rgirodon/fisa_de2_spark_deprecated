94d6fe5fb609310fbd0aa2b008f2fa9c40af726e
rgirodon <remy.girodon@gmail.com>
2020-04-09 16:01:47 +0200

    TP1

--- src/main/java/net/jgp/books/spark/ch02/lab100_csv_to_db/TP1_UsCensusTransformationApp.java
+++ src/main/java/net/jgp/books/spark/ch02/lab100_csv_to_db/TP1_UsCensusTransformationApp.java
@@ -44,29 +44,72 @@ public class TP1_UsCensusTransformationApp {
 
     // Renaming and dropping the columns we do not need
     
-       // drop columns GEO.id, resbase42010, respop72011, respop72012, respop72013, respop72014, respop72015, respop72016 (tip : dataset drop method)
-    
-       // rename column GEO.id2 -> id, GEO.display-label -> label, rescen42010 -> real2010, respop72010 -> est2010, respop72017 -> est2017 (tip : dataset withColumnRenamed method)
-    
+    // drop columns GEO.id, resbase42010, respop72011, respop72012, respop72013, respop72014, respop72015, respop72016 (tip : dataset drop method)
+    intermediateDf = intermediateDf
+            			.drop("GEO.id")
+            			.drop("resbase42010")
+            			.drop("respop72011")
+            			.drop("respop72012")
+            			.drop("respop72013")
+            			.drop("respop72014")
+            			.drop("respop72015")
+            			.drop("respop72016");
+            			    
+    // rename column GEO.id2 -> id, GEO.display-label -> label, rescen42010 -> real2010, respop72010 -> est2010, respop72017 -> est2017 (tip : dataset withColumnRenamed method)
+    intermediateDf = intermediateDf
+    					.withColumnRenamed("GEO.id2", "id")
+    					.withColumnRenamed("GEO.display-label", "label")
+    					.withColumnRenamed("rescen42010", "real2010")
+    					.withColumnRenamed("respop72010", "est2010")
+    					.withColumnRenamed("respop72017", "est2017");
     
     // Creates the additional columns
     
-       // create column countyState as an array from column label splitted around the "," (tip : dataset withColumn and col methods, and split function)
+    // create column countyState as an array from column label splitted around the "," (tip : dataset withColumn and col methods, and split function)
+    intermediateDf = intermediateDf
+						.withColumn("splittedLabel", split(intermediateDf.col("label"), ", "));
     
-          // create column state as 2nd part of column countyState (tip : column getItem method)
+    // create column state as 2nd part of column countyState (tip : column getItem method)
+    intermediateDf = intermediateDf
+    					.withColumn("state", intermediateDf.col("splittedLabel").getItem(1));
     
-          // create column county as 2nd part of column countyState (tip : column getItem method)
+    // create column county as 2nd part of column countyState (tip : column getItem method)
+    intermediateDf = intermediateDf
+						.withColumn("county", intermediateDf.col("splittedLabel").getItem(0));
     
-       // create column stateId from column id (tip : expr function)
+    intermediateDf = intermediateDf
+						.drop("splittedLabel")
+						.drop("label");
     
-       // create column countyId from column id (tip : expr function)
+    // create column stateId from column id (tip : expr function)
+    intermediateDf = intermediateDf
+    					.withColumn("stateId", expr("int(id / 1000)"));
     
-    
+    // create column countyId from column id (tip : expr function)
+    intermediateDf = intermediateDf
+						.withColumn("countyId", expr("id % 1000"));
+        
+    intermediateDf = intermediateDf
+    					.drop("id");
+    					
     // Compute new columns
 
-       // diff is est2010 - real2010 (tip : dataset withColumn method, and expr function)
+    // diff is est2010 - real2010 (tip : dataset withColumn method, and expr function)
+    intermediateDf = intermediateDf
+						.withColumn("diff", expr("est2010 - real2010"));
     
-       // growth is est2017-est2010 (tip : dataset withColumn method, and expr function)
-
+    // growth is est2017-est2010 (tip : dataset withColumn method, and expr function)
+    intermediateDf = intermediateDf
+						.withColumn("growth", expr("est2017 - est2010"));
+    
+    intermediateDf = intermediateDf
+						.drop("est2010")
+						.drop("real2010")
+						.drop("est2017")
+						.drop("real2017");
+    
+    intermediateDf = intermediateDf.select("stateId", "countyId", "state", "county", "diff", "growth");
+    
+    intermediateDf.show();	
   }
 }
